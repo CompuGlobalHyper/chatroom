49f3774a4eb1043855d5196d42998d6158a2cc1b
CompuGlobalHyper <phineascrisp@gmail.com>
2026-05-29 16:21:22 -0700

    Created empty routes and components and initialized vite and react

---
+++ chatroom-frontend/chatroom/src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <FrontPage />},
+      { path: '/login', element: <Login />},
+      { path: '/register', element: <Register />},
+      { path: '/home', element: <Home />},
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
